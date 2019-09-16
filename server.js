30c9d82b05dea2185e8c7dded96e17865cc8978d
Elias Rosales <eliasrosales88@gmgmail.com>
2019-09-17 00:52:56 +0200

    Setting up the workspace with eslint, husky, materialize, material icons and a node server

---
+++ server.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+const express = require("express");
+const { join } = require("path");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("."));
+app.get("/*", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () =>
+  console.log(`Listening PORT: ${PORT}`)
+);
+
