4a9da9ca3f324fde7958bd184259485b96cad601
Elias Rosales <eliasrosales88@gmgmail.com>
2019-09-19 03:53:05 +0200

    Adding description to beers and working on look and feel

--- src/js/beers.js
+++ src/js/beers.js
@@ -1,35 +1,82 @@
 "use strict";
 import  api from "./api.js";
+import { toggle } from "./ui.js";
 
 const { getBeers } = api();
 const mainSection = document.querySelector("#main-beers");
 
 // const {} = 
 
-const cardTemplate = `
+const cardTemplate = ({ name, description, image, likes, firstBrewed}) => `
   <div class="col s12 m6 l4">
-    <div class="card">
+    <div class="card hoverable">
       <div class="card-image">
-        <img src="./src/images/sample-1.jpg">
-        <span class="card-title">Card Title</span>
-        <a class="btn-floating halfway-fab waves-effect waves-light amber "><i class="material-icons grey-text text-darken-4">add</i></a>
+        <img class="activator beer-thumb" src="${image}">
+        <span class="btn-floating halfway-fab waves-effect waves-light amber"><i class="material-icons right grey-text text-darken-4">more_vert</i></span>
       </div>
       <div class="card-content">
-        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        <span class="card-title activator grey-text text-darken-4">${name}</span>
+        <p><a href="#">Full Review</a></p>
+        <p><i class="material-icons grey-text text-darken-4">thumb_up_alt</i> ${likes}</p>
+      </div>
+      <div class="card-reveal amber lighten-2">
+        <div class="card-close right"><i class="material-icons">close</i></div>
+        <div class="card-title grey-text text-darken-4">${name}</div>
+        <p>first brewed: <b>${firstBrewed}</b></p>
+        <p>${description}</p>
+        <p><a href="#">Full Review</a></p>
       </div>
     </div>
   </div>
 `;
 
 
+/**
+ * 
+ * @param {any} elementToInjectBeersDOM place where the beers will be injected
+ * @param {any} beers beers array
+ */
+const renderBeers = (elementToInjectBeersDOM, beers) => {
+  // Here is asigned the beer with the card template
+  const htmlBeer = beers.slice(0, 10).map(beer => {
+    return cardTemplate({ ...beer});
+  }).join("");
 
-
-const renderBeers = async () => {
-  const beers = await getBeers();
-  // eslint-disable-next-line no-unused-vars
-  beers.forEach(bear => {
-    mainSection.innerHTML += cardTemplate;
+  elementToInjectBeersDOM.innerHTML = htmlBeer;
+  
+  const cards = document.querySelectorAll(".card");
+  
+  cards.forEach(card => {
+    const descButton = card.querySelector("span.btn-floating");
+    const desc = card.querySelector(".card-reveal");
+    const closeDesc = card.querySelector(".card-close");
+    
+    descButton.addEventListener("click", () => toggle(desc, "reveal"));
+    closeDesc.addEventListener("click", () => toggle(desc, "reveal"));
+    
+    
+    
   });
 };
 
-renderBeers();
+
+
+/**
+ * Load beers and asign them in the DOM
+ */
+const loadBeers = async () => {
+  try {
+    const beers = await getBeers();
+
+    renderBeers(mainSection, beers);
+
+  } catch (err) {
+    console.log(err);
+  }
+  // // eslint-disable-next-line no-unused-vars
+  // beers.forEach(bear => {
+  //   mainSection.innerHTML += cardTemplate;
+  // });
+};
+
+loadBeers();
--- src/js/ui.js
+++ src/js/ui.js
@@ -1,1 +1,31 @@
 "use strict";
+console.log("UI Loaded");
+
+/**
+ * onHover class on hover
+ * @param {*} htmlElement Dom element
+ * @param {*} className css class
+ */
+export const onHover = (htmlElement, className) => {
+  
+  htmlElement.addEventListener("mouseenter", () => {
+    htmlElement.classList.toggle(className);
+  });
+  htmlElement.addEventListener("mouseleave", () => {
+    htmlElement.classList.toggle(className);
+  });
+};
+
+
+
+/**
+ * toggle class
+ * @param {*} htmlElement Dom element
+ * @param {*} className css class
+ */
+export const toggle = (htmlElement, className) => {
+  htmlElement.classList.toggle(className);
+};
+
+
+
